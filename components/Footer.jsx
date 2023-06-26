b5b77e1f010f03d248e31b2f9cda0e7c86ec825e
Luys Fernnando <luysfernnando@gmail.com>
2023-06-26 07:48:22 -0300

    feature(Footer): criado e estilizado
    Criação e estilização do Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,21 @@
+import css from '../styles/Footer.module.css';
+import {UilFacebook, UilGithub, UilInstagram} from "@iconscout/react-unicons";
+import Image from "next/image";
+import Logo from "../assets/LF_22.png";
 export default function Footer () {
     return (
-        <div>
-            Footer
+        <div className={css.container}>
+           <span>TODOS OS DIREITOS RESERVADOS</span>
+            <div className={css.social}>
+                <UilFacebook size={45}/>
+                <UilGithub size={45}/>
+                <UilInstagram size={45}/>
+            </div>
+
+            <div className={css.logo}>
+                <Image src = {Logo} alt = "" width={50} height={50}/>
+                <span>Pizzaria LF</span>
+            </div>
         </div>
     )
 };
